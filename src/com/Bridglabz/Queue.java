8234a70c517eaf5f5e046279d8befdd47d8c06a7
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-06 16:32:23 +0530

    add Queue

---
+++ src/com/Bridglabz/Queue.java
@@ -0,0 +1,19 @@
+package com.Bridglabz;
+
+public class Queue 
+{
+	private final LinkedList linkedList;
+	public Queue() {
+		linkedList=new LinkedList();
+	}
+	public void enQueue(int data) {
+		linkedList.insertAtFirst(data);
+	}
+	public void printQueue() {
+		linkedList.printList();
+	}
+	public void deQueue() {
+		linkedList.deleteLast();
+	}
+	
+}
